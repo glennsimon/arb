6a93f6aa83b383d1e8f954e3d45cd995d519e9cd
Glenn Simon <glenncochransimon@gmail.com>
2021-12-20 07:32:48 -0800

    on hold - maybe eliminate graphData?

--- stock-query.js
+++ stock-query.js
@@ -1,10 +1,20 @@
-const finnhub = require('finnhub');
+const candleBaseUrl = 'https://finnhub.io/api/v1/stock/candle?symbol=';
 
-const apiKey = finnhub.ApiClient.instance.authentications['api_key'];
-apiKey.apiKey = 'c6aifiiad3id24fn2m00';
-const finnhubClient = new finnhub.DefaultApi();
+const aYearAgo = 365 * 24 * 60 * 60;
+const now = Math.floor(Date.now() / 1000);
 
-finnhubClient.stockCandles('GOOG', '1', 1631025000, 1631025180, (error, data, response) => {
-  console.log('error: ' + error);
-  console.log(data);
+fetch(candleBaseUrl + symbol + '&resolution=' + 'M' + '&from=' + aYearAgo + '&to=' + now + '&token=c6aifiiad3id24fn2m00')
+.then((response) => {
+  return response.json();
+})
+.then((obj) => {
+  // load the results array
+  console.log(obj);
+  // results[symbol] = obj;
+  // if (symbols[0] in results && symbols[1] in results) {
+  //   processData();
+  // }
+})
+.catch((error) => {
+  console.error('Error fetching data: ', error);
 });
