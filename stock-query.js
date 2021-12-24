eb5f1f17fd4b4e1e075d901493e47bf596d6f1ab
Glenn Simon <glenncochransimon@gmail.com>
2021-12-24 10:41:58 -0800

    incorporated promises and async-await

--- stock-query.js
+++ stock-query.js
@@ -1,9 +1,12 @@
 const candleBaseUrl = 'https://finnhub.io/api/v1/stock/candle?symbol=';
 
-const aYearAgo = 365 * 24 * 60 * 60;
 const now = Math.floor(Date.now() / 1000);
 
-fetch(candleBaseUrl + symbol + '&resolution=' + 'M' + '&from=' + aYearAgo + '&to=' + now + '&token=c6aifiiad3id24fn2m00')
+const aYearAgo = now - 365 * 24 * 60 * 60;
+const thirtyDaysAgo = now - 30 * 24 * 60 * 60;
+const timeStamp = 0;
+
+fetch(candleBaseUrl + symbol + '&resolution=' + 'D' + '&from=' + thirtyDaysAgo + '&to=' + now + '&token=c6aifiiad3id24fn2m00')
 .then((response) => {
   return response.json();
 })
