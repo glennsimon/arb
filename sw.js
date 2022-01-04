3b4ca289f8d574a71c3909656d51e381660668fc
Glenn Simon <glenncochransimon@gmail.com>
2022-01-04 11:14:32 -0800

    added install event listener to sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,16 @@
-// TBD: populate service worker
+var CACHE_NAME = 'arb-cache-v1';
+var urlsToCache = [
+  // unused file, just to try to get the sw to work
+  'stock-query.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
